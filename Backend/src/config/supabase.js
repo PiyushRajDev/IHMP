df291620a380c098ce90263c2cfce539354d4b76
piyushGit1229 <varmapiyush864@gmail.com>
2025-03-07 00:26:05 +0530

    Database steup and Fast Api

--- Backend/src/config/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';  
-
-const supabaseUrl = 'https://qicdbfueadwfgtvjhfpe.supabase.co'; // Replace with your actual Supabase URL  
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpY2RiZnVlYWR3Zmd0dmpoZnBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExMTAzOTQsImV4cCI6MjA1NjY4NjM5NH0.jD75PlOzStPngIYaX4CrEWOWcYrJK2xkEu6HbAF4NNk'; // Replace with your actual Supabase Anon Key  
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);  
-
-export default supabase;  
